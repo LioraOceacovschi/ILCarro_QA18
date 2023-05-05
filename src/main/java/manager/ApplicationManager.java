aff2db63186e8b48d9cf47d6bd2d3d94a0315288
LioraOceacovschi <liora.skliar@gmail.com>
2023-05-06 02:59:12 +0300

    CW_15_Logger_Listener(Part1)

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,17 +3,24 @@ package manager;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    EventFiringWebDriver wd;
+    //WebDriver wd;
+     Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
 
-    WebDriver wd;
     HelperUser user;
     HelperSearch search;
 
-    public void init(){
-        wd = new ChromeDriver();
+    public void init() {
+       // wd = new ChromeDriver();
+        wd = new EventFiringWebDriver(new ChromeDriver());
+        wd.register(new MyListener());
         wd.manage().window().maximize();
         wd.navigate().to("https://ilcarro.web.app/search");
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -24,9 +31,12 @@ public class ApplicationManager {
     public HelperUser getUser() {
         return user;
     }
-    public  HelperSearch getSearch(){return search;}
 
-    public void stop(){
+    public HelperSearch getSearch() {
+        return search;
+    }
+
+    public void stop() {
 //        wd.quit();
     }
 
--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -3,23 +3,23 @@
 package tests;
 
 import models.User;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RegistrationTests extends TestBase{
+public class RegistrationTests extends TestBase {
 
     @BeforeMethod
-    public void precondition(){
-        if(app.getUser().isLogged()){
+    public void precondition() {
+        if (app.getUser().isLogged()) {
             app.getUser().logout();
         }
     }
+
     @Test
-    public void registrationPositiveTest(){
-        int i = (int)((System.currentTimeMillis() / 1000) % 3600);
+    public void registrationPositiveTest() {
+        int i = (int) ((System.currentTimeMillis() / 1000) % 3600);
 
         User user = new User()
                 .withName("Joe")
@@ -27,10 +27,13 @@ public class RegistrationTests extends TestBase{
                 .withEmail("joe" + i + "@mail.com")
                 .withPassword("$Asdf1234" + i);
 
+        logger.info("registrationPositiveTest starts with:  " + user.getEmail() + " & " + user.getPassword());
+
         app.getUser().openRegistrationForm();
         app.getUser().fillRegistrationForm(user);
         app.getUser().clickCheckbox();
         app.getUser().submitForm();
+        logger.info("registrationPositiveTest completed");
 
         Assert.assertTrue(app.getUser().isRegisteredSuccess());
 
@@ -51,7 +54,7 @@ public class RegistrationTests extends TestBase{
 //    }
 
     @AfterMethod
-    public void postCondition(){
+    public void postCondition() {
         app.getUser().clickOkButton();
     }
 }
